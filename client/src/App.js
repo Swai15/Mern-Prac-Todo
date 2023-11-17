55f9e10bfedaee1be799f6ac4e063ab82ce3bd8b
Swai15 <swaisilvano5@gmail.com>
2023-11-17 15:05:23 +0300

    Fetch and display data

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,39 @@
+import { useState, useEffect } from "react";
+const API_BASE = "http://localhost:5001";
+
 function App() {
+  const [todos, setTodos] = useState([]);
+
+  useEffect(() => {
+    getTodos();
+  }, []);
+
+  const getTodos = () => {
+    fetch(API_BASE + "/todos")
+      .then((res) => res.json())
+      .then((data) => {
+        setTodos(data);
+        console.log("Data fetched", data);
+      })
+      .catch((error) => console.error("Error: " + error.message));
+  };
+
   return (
     <div className="App">
-      <h1>React App</h1>
+      <div className="tasks-container">
+        <div className="heading">Todo List</div>
+        <div className="tasks-body">
+          {todos.map((todo) => {
+            return (
+              <div className="todo">
+                <div className="checkbox"></div>
+                <div className="text">{todo.text}</div>
+                <div className="delete-todo">x</div>
+              </div>
+            );
+          })}
+        </div>
+      </div>
     </div>
   );
 }
